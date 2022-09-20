2a957ca433233696896bd9ed9be658f2da44e16f
Murilo Silva <murilo.silva@ttmstech.com>
2022-09-20 17:16:02 -0300

    Alterando a árvore

--- arvore.js
+++ arvore.js
@@ -1,54 +1,97 @@
-const input = document.querySelector("input")
-const addBtn = document.querySelector("button.add")
+google.charts.load('current', {packages:["orgchart"]});
+google.charts.setOnLoadCallback(drawChart);
 
-class No {
-  constructor(valor) {
-    this.valor = valor
-    this.esquerdo = null
-    this.direito = null
-  }
+function Arvore( valor,galhoPai = null ){
+    this.galhoEsquerdo = null;
+    this.galhoDireito = null;
+    this.galhoPai = galhoPai;
+    this.valor = valor;
 }
 
-class Arvore {
-  constructor() {
-    this.raiz = null
-  }
+Arvore.prototype.add = function(valor){
+    if(valor == this.valor) return;
+    const vaiParaDireita = this.valor < valor;
+    
+    if (vaiParaDireita) {
+        if (!this.galhoDireito){
+            this.galhoDireito = new Arvore(valor, this);
+            return;
+        } 
+            this.galhoDireito.add(valor);
+            return;
+    } 
 
-  adicionar(valor) {
-    const novoNo = new No(valor)
+    if (!this.galhoEsquerdo){
+        this.galhoEsquerdo = new Arvore(valor, this);
+        return;
+    } 
+        this.galhoEsquerdo.add(valor);
+        return;
 
-    if (!this.raiz) {
-      this.raiz = novoNo
-    } else {
-      let atual = this.raiz
-      while (atual) {
-        if (valor < atual.valor) {
-          if (!atual.esquerdo) {
-            atual.esquerdo = novoNo
-            break
-          } else {
-            atual = atual.esquerdo
-          }
-        } else if (valor > atual.valor) {
-          if (!atual.direito) {
-            atual.direito = novoNo
-            break
-          } else {
-            atual = atual.direito
-          }
-        } else {
-          throw new Error("Tente um novo valor")
-        }
-      }
+}
+
+Arvore.prototype.ler = function(){
+
+    let arvore = [];
+
+    if(this.galhoDireito){
+        arvore = arvore.concat(this.galhoDireito.ler());
     }
 
-  }
+    arvore.push([
+        this.valor, 
+        this.galhoPai ? this.galhoPai.valor : '',
+        ''
+    ]);
+
+    if(this.galhoEsquerdo){
+        arvore = arvore.concat(this.galhoEsquerdo.ler())
+    }
+
+    return arvore;
+
 }
 
-const arvore = new Arvore()
+function drawChart() {
+    const data = new google.visualization.DataTable();
+    const valor = document.getElementById('valor');
+    const form = document.getElementById('form');
+    const arvore = new Arvore(null,null);
 
-addBtn.onclick = () => {
-    arvore.adicionar(input.value)
-    console.log(arvore)  
-    document.getElementById("arvore").innerHTML = "Sua árvore: <br>" + arvore
+
+    data.addColumn('string', 'Name');
+    data.addColumn('string', 'Manager');
+    data.addColumn('string', 'ToolTip');
+
+
+    data.addRows([]);
+
+
+    var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
+
+    chart.draw(data, {'allowHtml':true});
+
+
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        if(!arvore.valor){
+            arvore.valor = valor.value;
+        } else {
+            arvore.add(valor.value);
+        }
+
+        valor.value = '';
+
+        try{
+            data.removeRows(0, data.getNumberOfRows())
+        }finally{
+            data.addRows(arvore.ler());
+            chart.draw(data, {'allowHtml':true});
+        }
+
+    })
 }
+
+
